69467a158cc43d707e940aeef5674c88f47ea2e8
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-12 19:33:59 +0800

    Delete code directory

--- code/04_analysis_R/CCU013_colorscheme.R
+++
@@ -1,9 +0,0 @@
-mycolors <- data.frame(covid_phenotype = c("01_Covid_diagnosis", "01_Covid_positive", "02_Covid_admission", "03_ICU_admission", "03_NIV_treatment",
-                                           "03_IMV_treatment", "03_ECMO_treatment", "04_Fatal_with_covid_diagnosis", "04_Fatal_without_covid_diagnosis", "04-Covid_inpatient death"),
-                       color = c("#74c476", "#006d2c", "#fe9929", "#b3cde3", "#8c96c6", "#8856a7", "#810f7c", "#3182bd", "#1e60a3", "#154372"))
-
-mycolors2 <- data.frame(covid_phenotype = c("Positive test", "GP Diagnosis", "Hospitalisation", "Critical care", "Death"),
-                       color = c("#74c476", "#006d2c", "#fe9929", "#b3cde3", "#154372"))
-
-mycolors3 <- data.frame(covid_phenotype = c("Unaffected", "Positive test", "GP Diagnosis", "Hospitalisation", "Critical care", "Death"),
-                        color = c("#f0f4fa", "#74c476", "#006d2c", "#fe9929", "#b3cde3", "#154372"))
